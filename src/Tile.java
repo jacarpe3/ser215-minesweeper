a5717009688612ada3a367408301b49af179a5ab
Josh Carpenter <jacarpe3@outlook.com>
2017-11-17 22:14:44 -0700

    implemented Tile class ADT

--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,57 @@
+
+/**
+ * Tile object for Minesweeper grid locations
+ */
+
 public class Tile {
 
-    //TODO implement this
+    private boolean hasMine, isMarked, isSelected;
+
+    public Tile() {
+        this.hasMine = false;
+        this.isMarked = false;
+        this.isSelected = false;
+    }
+
+    /**
+     * @return boolean value representing if the tile is marked as containing a mine
+     */
+    public boolean isMarked() {
+        return isMarked;
+    }
+
+    /**
+     * @return boolean value representing if the tile has been selected (turned over) yet
+     */
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    /**
+     * @return boolean value representing if the tile is hiding a mine underneath
+     */
+    public boolean hasMine() {
+        return hasMine;
+    }
+
+    /**
+     * sets the isMarked variable
+     */
+    public void setMarked(boolean marked) {
+        this.isMarked = marked;
+    }
+
+    /**
+     * sets the isSelected variable
+     */
+    public void setSelected(boolean selected) {
+        this.isSelected = selected;
+    }
+
+    /**
+     * sets the hasMine variable
+     */
+    public void setMine(boolean mine) {
+        this.hasMine = mine;
+    }
 }
