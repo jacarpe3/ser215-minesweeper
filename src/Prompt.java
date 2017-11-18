5f17f9693cd632bdfa94cdd0012da8d136df17bb
Josh Carpenter <jacarpe3@outlook.com>
2017-11-17 23:01:06 -0700

    implemented playAgain and created stub method for makeSelection

--- src/Prompt.java
+++ src/Prompt.java
@@ -9,14 +9,14 @@ public class Prompt {
 
     private Prompt(){}
 
-    private static String invalid = "Invalid Entry!  Please try again...";
+    private static String invalidMsg = "Invalid Entry!  Please try again...";
 
     /**
      * Prompts the user for the desired level of difficulty and will ONLY accept an int value between 1 and 3
      * @return int value representing the desired level of difficulty
      */
     public static int getDifficulty() {
-        return getUserInt("Enter the desired level of difficulty: ", invalid, 1, 3);
+        return getUserInt("Enter the desired level of difficulty: ", invalidMsg, 1, 3);
     }
 
     /**
@@ -24,7 +24,41 @@ public class Prompt {
      * @return int value representing the desired level of difficulty
      */
     public static int selectAction() {
-        return getUserInt("Select an action: ", invalid, 1, 2);
+        return getUserInt("Select an action: ", invalidMsg, 1, 2);
+    }
+
+    /**
+     * Prompts user if they would like to play again
+     * @return boolean value referring to player's choice to play again
+     */
+    public static boolean playAgain() {
+        boolean invalid, playAgain = false;
+        String entry;
+        do {
+            invalid = false;
+            entry = getUserString("Would you like to play again? (Y or N): ");
+            if (entry.equalsIgnoreCase("Y")) {
+                playAgain = true;
+            } else if (entry.equalsIgnoreCase("N")) {
+                playAgain = false;
+            } else {
+                invalid = true;
+                System.out.println(invalidMsg);
+            }
+        } while (invalid);
+        return playAgain;
+    }
+
+    /**
+     * Prompts the user to enter the tile they wish to select (or mark)
+     * @return a int[] where the value at index 0 is the row and the value at index 1 is the column
+     */
+    public static int[] makeSelection() {
+        int[] location = new int[2];
+
+        //TODO Implement prompt and validation of selection entry
+
+        return location;
     }
 
     /**
@@ -32,7 +66,7 @@ public class Prompt {
      * characters.  The integer must be >= the minimum value specified
      * and <= the max value specified.
      * @param prompt the prompt you wish to be displayed to the user
-     * @param invalid the message displayed when an invalid entry is given
+     * @param invalid the message displayed when an invalidMsg entry is given
      * @param min the minimum integer value to be accepted
      * @param max the maximum integer value to be accepted
      * @return integer value from user
@@ -60,4 +94,16 @@ public class Prompt {
         } while (isInvalid);
         return input;
     }
+
+    /**
+     * Prompts user for input and returns a string
+     * @param prompt the prompt you wish to be displayed to the user
+     * @return String value of the input from the user
+     */
+    private static String getUserString(String prompt) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(prompt);
+        return scan.nextLine();
+    }
+
 }
